b0494c1a55855d128037addacc0ebcfeb9524f5c
anne-marziale <annemarziale@hotmail.com>
2022-01-26 23:47:25 +0100

    memberlist from database is fetch

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,14 +4,15 @@ import './Header.css';
 function Header() {
   return (
     <div className="Header">
-        <div className="header-container">
-          <img
-            className="logo"
-            src="https://www.wildcodeschool.com/assets/logo_main-e4f3f744c8e717f1b7df3858dce55a86c63d4766d5d9a7f454250145f097c2fe.png"
-            alt="Wild Code School logo"
-          />
-          <div className='title-header'>Les Argonautes</div>
-        </div>
+      <div className="header-container">
+        <img
+          className="logo"
+          src="https://www.wildcodeschool.com/assets/logo_main-e4f3f744c8e717f1b7df3858dce55a86c63d4766d5d9a7f454250145f097c2fe.png"
+          alt="Wild Code School logo"
+        />
+
+        <h2 className="title-header">Les Argonautes</h2>
+      </div>
     </div>
   );
 }
